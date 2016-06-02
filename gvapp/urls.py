cbbfc1772a2af4a429d6d9dc2bd55c381185fb96
thelucasrovere <thelucasrovere@gmail.com>
2016-06-02 16:10:08 -0400

    cadastro 1

--- gvapp/urls.py
+++ gvapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^campanhas/$', views.campanhas, name='campanhas'),
     url(r'^campanhas/details/$', views.campanha_details, name='campanha_details'),
     url(r'^$', views.index, name="index"),
+    url(r'^organizador/cadastro/$', view.cadastroorg, name='Cadastro'),
 ]
