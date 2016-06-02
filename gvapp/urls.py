da5b8767f0fffef3a2cda4c3c1b388c99903933b
thelucasrovere <thelucasrovere@gmail.com>
2016-06-02 18:11:59 -0400

    fuck

--- gvapp/urls.py
+++ gvapp/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^organizador/cadastro/$', views.cadastro_org, name='Cadastro'),
     url(r'^campanhas/$', views.campanhas, name='campanhas'),
     url(r'^campanhas/details/$', views.campanha_details, name='campanha_details'),
     url(r'^$', views.index, name="index"),
-    url(r'^organizador/cadastro/$', view.cadastroorg, name='Cadastro'),
 ]
