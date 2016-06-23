6a9ba5ef8e7ba59615a4b78e0d3b3c06eae47855
guicaiol <gcaixetaoliveira@gmail.com>
2016-06-23 19:52:34 -0300

    added two-step registration for donors

--- gvapp/doaviews.py
+++ gvapp/doaviews.py
@@ -6,17 +6,31 @@ from django.core.context_processors import csrf
 
 @csrf_protect
 def cadastro_doador(request):
+
     if request.method == 'POST':
-        print "tenta cadastro doador"
-   
-        return HttpResponse("ok doador")
         
-    else:
-    
-        template = loader.get_template('cadastro_doador.html')
-        context = {
-            'page_title': 'Cadastro Doador'
-        }
-        context.update(csrf(request))
-    
+        step = request.POST.get('step', '')
+        #fbUserId = request.POST.get('fbUserId', '')
+
+        #return HttpResponse("user id= "+fbUserId)
+
+        if step == '1':
+
+            template = loader.get_template('cadastro_doador_2.html')
+            context = {
+                'page_title': 'Cadastro Organizador',
+            }
+            context.update(csrf(request))
+
+            return HttpResponse(template.render(context))
+            
+        elif step =='2':
+            return HttpResponse("step 2!")
+        
+    template = loader.get_template('cadastro_doador.html')
+    context = {
+        'page_title': 'Cadastro Doador',
+    }
+    context.update(csrf(request))
+
     return HttpResponse(template.render(context))
